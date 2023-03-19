a5b0c8e8514f736d72315e973b3ff377b13d220c
xufangbo <xufangbo@hotmail.com>
2023-03-19 14:17:46 +0000

    xx

--- edc/lwdee/DDO.h
+++ edc/lwdee/DDO.h
@@ -10,26 +10,15 @@
 typedef UhconnDdoRef DDOId;
 
 class DDO {
-
  public:
   DDOId ddoId;
 
  public:
   DDO() {}
   DDO(DDOId id) : ddoId(id) {}
+
+ public:
   void write(ByteSpan_ref datablock);
   ByteSpan_ref read();
   void release();
 };
-
-// class DdoManager {
-//  private:
-//   static std::atomic<DDOId> incresement;
-//   static std::map<DDOId, ByteSpan_ref> ddos;
-
-//  public:
-//   static DDOId generateId() { return ++incresement; };
-//   static void set(DDOId id, ByteSpan_ref blockdata);
-//   static ByteSpan_ref get(DDOId id);
-//   static void remove(DDOId id);
-// };
