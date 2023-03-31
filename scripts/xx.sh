6365f8f4f290b1c531dd20243403437a53a9477b
xufangbo <xufangbo@hotmail.com>
2023-03-31 15:10:19 +0800

    xx

--- scripts/xx.sh
+++ scripts/xx.sh
@@ -5,3 +5,9 @@ ssh root@k8s-node03 "docker ps"
 ssh root@k8s-node04 "docker ps"
 ssh root@k8s-node05 "docker ps"
 
+
+
+ssh root@k8s-node01 "docker logs terasort1"
+
+ssh root@k8s-node02 "docker logs terasort3"
+
