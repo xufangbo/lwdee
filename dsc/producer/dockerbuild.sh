2d0f493dca64f8ec4272b036bb61fe637696d941
xufangbo <xufangbo@hotmail.com>
2023-04-06 11:49:55 +0800

    xx

--- dsc/producer/dockerbuild.sh
+++ dsc/producer/dockerbuild.sh
@@ -1,4 +1,7 @@
-app=terasort
+app=dscproducer
+version=v1.0.1
+
+cp ../../build/dsc/producer/$app ./
 
 docker stop $app
 docker rm $app
@@ -6,12 +9,12 @@ docker image rm $app
 docker build -t $app .
 # docker run --name $app -p 16501:16501 -e nodename=node3 -v /home/kevin/git/lwdee/log1:/home/log -d $app 
 
-# docker exec -it  terasort2 /bin/bash
+# docker exec -it  dscproducer2 /bin/bash
 
 # # 阿里与云个人镜像：公网
 # docker login --username=xufa****@***.com registry.cn-beijing.aliyuncs.com
 
-docker $app terasort registry.cn-beijing.aliyuncs.com/xufangbo/$app:v1.0.18
-docker push registry.cn-beijing.aliyuncs.com/xufangbo/$app:v1.0.18
+docker tag $app registry.cn-beijing.aliyuncs.com/xufangbo/$app:$version
+docker push registry.cn-beijing.aliyuncs.com/xufangbo/$app:$version
 
-# docker pull registry.cn-beijing.aliyuncs.com/xufangbo/terasort:v1.0.18
+# docker pull registry.cn-beijing.aliyuncs.com/xufangbo/dscproducer:v1.0.18
