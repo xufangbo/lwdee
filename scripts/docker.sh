abd6c81c4711d7a9cc93690566626ccc0149b2aa
xufangbo <xufangbo@hotmail.com>
2023-04-07 10:13:26 +0800

    xx

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,9 +8,9 @@ docker rm terasort3
 
 
 
-ssh root@localhost "docker run --name terasort1  -e nodename=node1 --net=host -p 6301:6301 -p 6311:6311 -v /home/kevin/git/lwdee/log:/home/terasort/log -v /home/kevin/git/lwdee/data:/home/terasort/data -v /home/kevin/git/lwdee/config:/home/terasort/config -d registry.cn-beijing.aliyuncs.com/xufangbo/terasort:v1.0.18"
+ssh root@localhost "docker run --name terasort1  -e nodename=node1 --net=host -p 6401:6401 -p 6411:6411 -v /home/kevin/git/lwdee/log:/home/terasort/log -v /home/kevin/git/lwdee/data:/home/terasort/data -v /home/kevin/git/lwdee/config:/home/terasort/config -d registry.cn-beijing.aliyuncs.com/xufangbo/terasort:v1.0.18"
 
 
-ssh root@localhost "docker run --name terasort2  -e nodename=node2 --net=host -p 6302:6302 -p 6312:6312 -v /home/kevin/git/lwdee/log:/home/terasort/log -v /home/kevin/git/lwdee/data:/home/terasort/data -v /home/kevin/git/lwdee/config:/home/terasort/config -d registry.cn-beijing.aliyuncs.com/xufangbo/terasort:v1.0.18"
-ssh root@localhost "docker run --name terasort3  -e nodename=node3 --net=host -p 6303:6303 -p 6313:6313 -v /home/kevin/git/lwdee/log:/home/terasort/log -v /home/kevin/git/lwdee/data:/home/terasort/data -v /home/kevin/git/lwdee/config:/home/terasort/config -d registry.cn-beijing.aliyuncs.com/xufangbo/terasort:v1.0.18"
+ssh root@localhost "docker run --name terasort2  -e nodename=node2 --net=host -p 6402:6402 -p 6412:6412 -v /home/kevin/git/lwdee/log:/home/terasort/log -v /home/kevin/git/lwdee/data:/home/terasort/data -v /home/kevin/git/lwdee/config:/home/terasort/config -d registry.cn-beijing.aliyuncs.com/xufangbo/terasort:v1.0.18"
+ssh root@localhost "docker run --name terasort3  -e nodename=node3 --net=host -p 6403:6403 -p 6413:6413 -v /home/kevin/git/lwdee/log:/home/terasort/log -v /home/kevin/git/lwdee/data:/home/terasort/data -v /home/kevin/git/lwdee/config:/home/terasort/config -d registry.cn-beijing.aliyuncs.com/xufangbo/terasort:v1.0.18"
 
